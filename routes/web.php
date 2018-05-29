789939e6c56658d2be07f1498822dbed99413f4c
Godfrey Chiu <godfrey.chiu@catenamedia.com>
2018-05-29 12:10:41 +0100

    added ReadOnlyBase to show example of dependency injection

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return '<h2>Landon App Page</h2>';
 });
+
+Route::get('/about', function () {
+    // returning an array as a response - JSON, basic API response
+    $response_arr = [];
+    $response_arr['author'] = 'G Man';
+    $response_arr['version'] = '0.0.1'; 
+    return $response_arr;
+});
+
+Route::get('/', function () {
+    // passing data into the view
+    $data = [];
+    $data['version'] = 'v0.0.1';
+    return view('welcome', $data);
+});
+
+Route::get('/di', 'ClientController@di');
